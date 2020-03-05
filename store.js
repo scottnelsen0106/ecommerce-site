ff54836f1659a70084de4d5443bddeb70fd57aef
Scott Nelsen <snelsen0106@gmail.com>
2020-03-04 21:23:25 -0600

    added html and images with bootstrap

---
+++ store.js
@@ -0,0 +1,8 @@
+// I need to be able to display all of my images that have prices on them.
+
+// I need to be able to have an Add To Cart button for each item that when clicked will add the item to a cart.
+
+// When I view my cart, I need to have all the items that I clicked on viewed. those items need a proceed to checkout button and also be able to remove items from my cart.
+
+// In my view cart page, I need to have a proceed to checkout button that will send an alert that my order shipped. I also need an option on this that can decline a checkout that will send me back to the store page.
+
